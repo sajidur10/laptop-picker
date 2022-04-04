c1023fcf8b106179d73b9627ff9c43348594813f
sajidur10 <msrrahatbd1@gmail.com>
2022-04-04 22:56:55 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Blogs from './components/Blogs/Blogs';
 import About from './components/About/About';
 import Notfound from './components/Notfound/Notfound';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<Notfound></Notfound>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright © 2022 Laptop Picker. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
