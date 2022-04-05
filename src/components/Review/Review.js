4a49e5178f97a1beb8e5701431ae074b4563454e
sajidur10 <msrrahatbd1@gmail.com>
2022-04-05 23:47:44 +0600

    bug fixed

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,12 +2,11 @@ import React from 'react';
 import './Review.css'
 
 const Review = ({ cusReview }) => {
-    console.log(cusReview)
 
     const { ratings, review, time, name, picture } = cusReview;
     return (
         <div className='review-container'>
-            <div className='review  flex items-center flex-col border h-96 p-5 drop-shadow-lg'>
+            <div className='review  flex items-center flex-col border p-5 drop-shadow-lg'>
                 <img className='w-20' src={picture} alt=""></img>
                 <div className='review-info'>
                     <p className='font-bold'>{name}</p>
